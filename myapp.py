428f3c2c6943606c1a8c48bfb3e579e02f0e45a2
Ankey <amanyankey049@gmail.com>
2023-02-21 15:38:59 +0000

    '2102'

--- myapp.py
+++ myapp.py
@@ -479,6 +479,11 @@ try:
             MONTANT += lISTE_MONTANT
             NUMEROS += lISTE_NUMEROS
 
+        st.write('1')
+        st.write(PRENOMS_NUM)
+        st.write(DATE)
+        st.write(MONTANT)
+        st.write(NUMEROS)
         data = pd.DataFrame(list(zip(PRENOMS_NUM, DATE, MONTANT,NUMEROS)), columns=['TEXT', 'DATE1', 'MONATANT1','NUMEROS'])
 
         data['PRENOMS'] = data['TEXT'].str.split(' ').str[0]
@@ -702,6 +707,11 @@ except:
             MONTANT += lISTE_MONTANT
             NUMEROS += lISTE_NUMEROS
 
+        st.write('2')
+        st.write(PRENOMS_NUM)
+        st.write(DATE)
+        st.write(MONTANT)
+        st.write(NUMEROS)
         data = pd.DataFrame(list(zip(PRENOMS_NUM, DATE, MONTANT,NUMEROS)), columns=['TEXT', 'DATE1', 'MONATANT1','NUMEROS'])
         data['PRENOMS'] = data['TEXT'].str.split(' ').str[0]
         data['TRANSACTION'] = data['PRENOMS'].apply(get_TRANSACTION)
